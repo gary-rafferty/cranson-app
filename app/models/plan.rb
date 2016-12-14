e2e51f7c227111de5f21829d01194b943c100f7e
Gary Rafferty <gary.rafferty@gmail.com>
2016-12-14 20:01:58 +0000

    add some other scopes for plans

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,4 +2,8 @@ class Plan < ApplicationRecord
   validates :status, :description, :reference, :location, :reference, :registration_date, presence: true
 
   scope :decided, -> { where(status: 'Decided' ) }
+  scope :invalid_or_withdrawn, -> { where(status: 'Invalid or Withdrawn') }
+  scope :unknown, -> { where(status: 'Current status not assigned in APAS') }
+  scope :pending, -> { where(status: 'Pending') }
+  scope :on_appeal, -> { where(status: 'On Appeal') }
 end
--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -15,12 +15,43 @@ RSpec.describe Plan, type: :model do
   end
 
   describe 'scopes' do
-    describe '#decided' do
-      let!(:decided)   { create(:plan) }
-      let!(:undecided) { create(:plan, status: 'Undecided') }
+    let!(:decided) { create(:plan) }
 
+    describe '#decided' do
       it 'retrieves only plans with decisions' do
         expect(Plan.decided.first).to eq decided
+      end
+    end
+
+    describe '#invalid_or_withdrawn' do
+      let!(:invalid) { create(:plan, status: 'Invalid or Withdrawn') }
+
+      it 'retrieves only invalid or withdrawn plans' do
+        expect(Plan.invalid_or_withdrawn.first).to eq invalid
+      end
+    end
+
+    describe '#unknown' do
+      let!(:unknown) { create(:plan, status: 'Current status not assigned in APAS') }
+
+      it 'retrieves only unknown plans' do
+        expect(Plan.unknown.first).to eq unknown
+      end
+    end
+
+    describe '#pending' do
+      let!(:pending) { create(:plan, status: 'Pending') }
+
+      it 'retrieves only pending plans' do
+        expect(Plan.pending.first).to eq pending
+      end
+    end
+
+    describe '#on_appeal' do
+      let!(:on_appeal) { create(:plan, status: 'On Appeal') }
+
+      it 'retrieves only plans on appeal' do
+        expect(Plan.on_appeal.first).to eq on_appeal
       end
     end
   end
