ed1a401c5ada9d86c9fe0c42ede00069f1a54df8
Gary Rafferty <gary.rafferty@gmail.com>
2018-02-13 23:13:32 +0000

    adds dcc to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,4 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 Authority.find_or_create_by(name: 'Fingal County Council')
+Authority.find_or_create_by(name: 'Dublin City Council')
